db6238f68602e84a7492feae6c87d2a9fffd1920
VenkatNan <nanditavenkat@gmail.com>
2020-12-17 15:16:56 -0600

    remake embedd

--- models/character.js
+++ models/character.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const skillSchema = new Schema({
+
+})
+const abilityScoreSchema = new Schema({
+   con :Number,
+   dex :Number,
+   wis :Number,
+   int :Number,
+   str :Number,
+   luck :Number,
+   char :Number,
+
+})
+const statsSchema = new Schema({
+    hp:Number,
+    ac:Number,
+    speed:Number,
+})
+
 const charSchema= new Schema({
-    name: String,
+    name: {Type: String , required:true},
+    race:{Type: String , required:true},
+    level: {Type: Number , required:true},
+    skills:[skillSchema],
+    abilityScore:[abilityScoreSchema],
+    stats:[statsSchema],
+
     
 },{
         timestamps:true
