c7ae7aa00e7ba24a7dcb51621dd0a70ab5f48e64
VenkatNan <nanditavenkat@gmail.com>
2020-12-17 19:38:30 -0600

    index fixed lol

--- controllers/characters.js
+++ controllers/characters.js
@@ -7,7 +7,9 @@ module.exports = {
   }
 
   function index(req,res){
-
+    Char.find({}, function(err, chars) {
+        res.render('characters', {title: "All characters", chars})
+      })
   }
 
   function addChar(req,res){
